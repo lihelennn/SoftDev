2bc3212eac1f51d7ddca5b441334f2c4359e9b34
lihelennn <helen82198@gmail.com>
2015-10-12 22:58:47 -0400

    donald login pg

---
+++ classwork/donald/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, session, redirect,url_for
+
+app = Flask(__name__)
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.secret_key="hello"
+    app.run(host='0.0.0.0', port=8000)
